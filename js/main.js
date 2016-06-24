0578bccfd1e6708c2be9d6531f035551455ac3e9
qwazik <qwazik21@gmail.com>
2016-06-24 15:45:17 +0300

    first

---
+++ js/main.js
@@ -0,0 +1,125 @@
+//ajax message
+	    $('form').submit(function(){
+	    var dataForm = $(this).serialize();
+	    $.ajax({
+	        method : 'POST',
+	        url: '../mail.php',
+	        data: dataForm
+	    })
+	    .success(function() {
+	        alert('Спасибо за сообщение! Мы с вами обязательно свяжемся.')
+	    });
+	    return false;
+	})
+
+//slide anchor
+jQuery(document).ready(function() {
+    jQuery("a.scrollto").click(function () {
+        elementClick = jQuery(this).attr("href")
+        destination = jQuery(elementClick).offset().top;
+        jQuery("html:not(:animated),body:not(:animated)").animate({scrollTop: destination}, 1100);
+        return false;
+    });
+});
+
+$('#filter input[type="reset"]').click(function(){
+	$('#filter .jq-checkbox.checked').removeClass('checked');
+})
+$('#sort, #pagi, #viewOutput').on('click', 'li', function(){
+	$(this).addClass('active');
+	$(this).siblings().removeClass('active');
+	return false;
+})
+
+$('#viewsType .view__tabs, #viewsType .view__rows').click(function(){
+	$(this).addClass('active');
+	$(this).siblings().removeClass('active');
+	viewsType();
+	return false;
+})
+
+function viewsType(){
+	if ($('.view__tabs').hasClass('active')) {$('.catalog__main').removeClass('list').addClass('tabs')}
+	if ($('.view__rows').hasClass('active')) {$('.catalog__main').removeClass('tabs').addClass('list')}
+
+}
+
+//rating stars
+
+$('.star').hover(function(){
+	starToActive($(this).index() + 1, $(this));
+	console.log($(this).index());
+}, function(){
+	$(this).siblings().add($(this)).removeClass('star--hover');
+})
+
+function starToActive(cst, el){
+	for(i = 0; i < cst; ++i){
+		$(el).siblings().add($(el)).eq(i).addClass('star--hover');
+		console.log(el, i);
+	}
+}
+
+$(this).on('click', function(){
+	$(this).snippet
+})
+
+$('#cartCol .minus').click(function(){
+	var curVal = parseInt($(this).siblings('input').val());
+	curVal--;
+	if (curVal >= 1) {
+		$(this).siblings('input').val(curVal--); 
+		$(this).closest('.cart__footer').find('.priceColDesc').text($(this).siblings('input').val());
+	}
+})
+
+$('#cartCol .plus').click(function(){
+	var curVal = parseInt($(this).siblings('input').val());
+	curVal++;
+	if (curVal >= 1) {
+		$(this).siblings('input').val(curVal++); 
+		$(this).closest('.cart__footer').find('.priceColDesc').text($(this).siblings('input').val());
+	}
+})
+
+$('.checkbox__row label').click(function(){
+	$(this).parent().toggleClass('checked');
+	console.log(this);
+})
+
+$('#tovarInfoTabs .tab__controls li').click(function(){
+	var tabData = $(this).find('a').attr('href');
+	tabData = '.'+tabData;
+	console.log(tabData);
+	$(this).siblings().removeClass('active');
+	$(this).addClass('active');
+	$('#tovarInfoTabs .tab__content').removeClass('active').hide();
+	$('#tovarInfoTabs .tab__content'+tabData+'').addClass('active').fadeIn(300);
+	return false;
+})
+
+$('.header__menu-teh').on('mouseleave', function(){
+	$('.submenu-teh').fadeOut(300);
+	$('.menu-teh .menu li').removeClass('active');
+})
+
+$('.menu-teh .menu li').on('mouseover', function(e){
+	e.preventDefault();
+	$(this).siblings().removeClass('active');
+	$(this).addClass('active');
+	var submenuId = $(this).find('a').attr('href');
+	openSubmenu(submenuId);
+
+})
+
+var openSubmenu = function(id){
+	$('.submenu-teh').fadeIn(300);
+	$('.submenu__item').hide();
+	$('.submenu__item#'+ id +'').show();
+	return false;
+}
+
+$('#payMethod label').on('click', function(e){
+	$(this).addClass('active');
+	$(this).siblings().removeClass('active');
+})
---
+++ js/plugins.js
@@ -0,0 +1,165 @@
+/*===============================================
+plugins
+===============================================*/
+
+// Avoid `console` errors in browsers that lack a console.
+(function() {
+    var method;
+    var noop = function () {};
+    var methods = [
+        'assert', 'clear', 'count', 'debug', 'dir', 'dirxml', 'error',
+        'exception', 'group', 'groupCollapsed', 'groupEnd', 'info', 'log',
+        'markTimeline', 'profile', 'profileEnd', 'table', 'time', 'timeEnd',
+        'timeline', 'timelineEnd', 'timeStamp', 'trace', 'warn'
+    ];
+    var length = methods.length;
+    var console = (window.console = window.console || {});
+
+    while (length--) {
+        method = methods[length];
+
+        // Only stub undefined methods.
+        if (!console[method]) {
+            console[method] = noop;
+        }
+    }
+}());
+
+//myPlugins
+  ;(function($){
+    $.fn.qTabs = function(){
+        var global = this;
+        global.find('.tabs-content__item').hide();
+        global.find('.tabs-content__item.active').show();
+        $(this).find('.tabs-nav li').click(function(){
+            $(this).addClass('active');
+            $(this).siblings().removeClass('active');
+            var data = $(this).find('a').attr('href');
+            $(global).find('.tabs-content__item').hide().removeClass('active');
+            $(global).find('.tabs-content__item' + data + '').fadeIn(300).addClass('active');
+            return false;
+        })
+    }
+
+    $.fn.qToggle = function(){
+        var global = this;
+        $(this).click(function(e){
+            e.preventDefault();
+            $(this).addClass('active');
+            $(this).siblings().removeClass('active');
+        })
+    }
+  }(jQuery));
+
+
+// Place any jQuery/helper plugins in here.
+var tovarCarouselCollection = [];
+
+$(document).ready(function(){
+    $('#pointTabs').qTabs();  //tabinit
+    $('.fancybox').fancybox(); //fancyboxinit
+    $('.point-list__item').qToggle();
+    $('#mainSlider').bxSlider({
+        controls: false,
+        auto: true,
+        pause: 10000
+    });
+
+for(i = 0; i < $('#tovarsCarousel .tovars__category').length; i++){
+    tovarCarouselCollection[i] = $('#tovarsCarousel .tovars__category').eq(i).bxSlider({
+        controls: true,
+        // auto: true,
+        pause: 10000,
+        slideWidth: 322,
+        minSlides: 4,
+        maxSlides: 4,
+    });
+}
+
+$('#partnersCarousel').bxSlider({
+    controls: true,
+    auto: true,
+    pause: 10000,
+    slideWidth: 322,
+    minSlides: 4,
+    maxSlides: 4,
+    pager:false
+})
+
+$('#recentlyCarousel, #carouselWidth').bxSlider({
+    controls: true,
+    auto: true,
+    pause: 10000,
+    slideWidth: 322,
+    minSlides: 3,
+    maxSlides: 4,
+    pager:false
+})
+   
+    $('.main__slider').css('visibility','visible');
+})
+
+$('#tovarsNav').on('click', 'li', function(){
+    $(this).addClass('active');
+    $(this).siblings().removeClass('active');
+    var num = $(this).index()
+    checkCategoryActive(num);
+})
+
+
+//big photo ===========================================================================
+var bigPhotoList = $('#photoList').bxSlider({
+    mode: 'vertical',
+    pager: false,
+    controls: false,
+    minSlides: 3,
+    maxSlides: 3,
+    slideWidth: 84,
+    slideHeight: 84,
+})
+$('#bigPhotoColntrols .top').click(function(){
+    bigPhotoList.goToPrevSlide();
+})
+$('#bigPhotoColntrols .bot').click(function(){
+    bigPhotoList.goToNextSlide();
+})
+$('#photoList').on('click', 'a', function(){
+    $(this).parent().addClass('active');
+    $(this).parent().siblings().removeClass('active');
+    $('#mainPhoto').attr('src', $(this).attr('href'));
+    return false;
+})
+
+
+
+$(document).ready(function(){
+    $('.photo__list').css('visibility', 'visible');
+})
+
+// =====================================================================================
+
+function checkCategoryActive(num){
+    $('.tovars__carousel').eq(num).siblings().hide().removeClass('active');
+    $('.tovars__carousel').eq(num).fadeIn(500).addClass('active');
+    tovarCarouselCollection[num].reloadSlider()
+}
+
+//slider
+  $(function() {
+    $( "#filterSliderRange" ).slider({
+      range: true,
+      min: 5000,
+      max: 10000,
+      values: [ 7000, 10000 ],
+      slide: function( event, ui ) {
+        $( "#priceFrom" ).val(ui.values[ 0 ]);  
+        $( "#priceTo" ).val(ui.values[ 1 ]); 
+      }
+    });
+    $( "#priceFrom" ).val($( "#filterSliderRange" ).slider('values', 0));  
+    $( "#priceTo" ).val($( "#filterSliderRange" ).slider('values', 1)); 
+
+    $('#filter input[type="checkbox"], .cart__right input, .tovar__pay input').styler();
+  });
+
+  
